3f6f5e23a340361aa912d153af2aca46f357448b
Kevin J <6829515+kmjones1979@users.noreply.github.com>
2025-02-22 14:03:37 -0700

    add files for foundational example

--- subgraph/src/mappings.ts
+++ subgraph/src/mappings.ts
@@ -1,15 +1,25 @@
 import { Protobuf } from "as-proto/assembly";
 import { Transactions as protoTransactions } from "./pb/sf/substreams/starknet/type/v1/Transactions";
-import { MyEntity } from "../generated/schema";
-import { BigInt, log, crypto, Bytes} from "@graphprotocol/graph-ts";
+import { MyEntity, Transfer, Approval } from "../generated/schema";
+import { BigInt, log, crypto, Bytes } from "@graphprotocol/graph-ts";
 
 export function handleTriggers(bytes: Uint8Array): void {
-  const input = Protobuf.decode<protoTransactions>(bytes, protoTransactions.decode);
-  
-  // No ID field has been found in the proto input...
-  // The input has been hashed to create a unique ID, replace it with the field you want to use as ID
-  const inputHash = crypto.keccak256(Bytes.fromUint8Array(bytes)).toHexString();
-  let entity = new MyEntity(inputHash);
+    const input = Protobuf.decode<protoTransactions>(
+        bytes,
+        protoTransactions.decode
+    );
 
-  entity.save();
+    // No ID field has been found in the proto input...
+    // The input has been hashed to create a unique ID, replace it with the field you want to use as ID
+    const inputHash = crypto
+        .keccak256(Bytes.fromUint8Array(bytes))
+        .toHexString();
+
+    let transfer = new Transfer(Bytes.fromHexString(inputHash));
+    transfer.from = Bytes.fromHexString(transfer.from.toHexString());
+    transfer.to = Bytes.fromHexString(transfer.to.toHexString());
+    transfer.value = BigInt.fromString(transfer.value.toString());
+    transfer.timestamp = transfer.timestamp;
+    transfer.blockNumber = BigInt.fromString(transfer.blockNumber.toString());
+    transfer.save();
 }
